f6d808ed96904cd923809900fa4d314da9fdff89
Andreas Pauley <apauley+github@gmail.com>
2009-07-24 23:13:47 +0200

    Sudoku: refactor specs

--- Ruby/Sudoku/sudoku_puzzle_spec.rb
+++ Ruby/Sudoku/sudoku_puzzle_spec.rb
@@ -1,15 +1,18 @@
 require 'sudoku_puzzle'
 
-describe SudokuPuzzle do
-    it "should have a 9x9 grid by default" do
-        puzzle = SudokuPuzzle.new(9)
-        puzzle.grid.row_size().should == 9
-        puzzle.grid.column_size().should == 9
-    end
+def check_size(puzzle, expected_size)
+  puzzle.grid.row_size().should == expected_size
+  puzzle.grid.column_size().should == expected_size
+end
 
-    it "should allow the construction of puzzles with sizes" do
-        puzzle = SudokuPuzzle.new(2)
-        puzzle.grid.row_size().should == 2
-        puzzle.grid.column_size().should == 2
-    end
+describe SudokuPuzzle do
+  it "should have a 9x9 grid by default" do
+    puzzle = SudokuPuzzle.new(9)
+    check_size(puzzle, 9)
+  end
+
+  it "should allow the construction of puzzles with different sizes" do
+    puzzle = SudokuPuzzle.new(2)
+    check_size(puzzle, 2)
+  end
 end
