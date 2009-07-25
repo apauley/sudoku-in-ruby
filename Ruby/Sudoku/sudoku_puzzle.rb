4cbbf624111433e2dcfc16ebe34037523ccd8fec
Andreas Pauley <apauley+github@gmail.com>
2009-07-25 22:29:37 +0200

    Sudoku: validate grid entries

--- Ruby/Sudoku/sudoku_puzzle.rb
+++ Ruby/Sudoku/sudoku_puzzle.rb
@@ -3,15 +3,29 @@ require 'matrix'
 class SudokuPuzzle
   def initialize(grid_rows)
     @grid = Matrix[*grid_rows]
-    validate_grid()
+    validate
   end
 
-  def validate_grid
+  def validate
     if not @grid.square?
       raise
     end
+    validate_entries(@grid.to_a)
   end
 
+  def validate_entries(an_array)
+    for i in 0...an_array.length
+      for j in 0...an_array[i].length
+        if an_array[i][j] > size
+          raise
+        end
+      end
+    end
+  end
+
+  def size
+    grid.row_size()
+  end
   def grid
     @grid
   end
--- Ruby/Sudoku/sudoku_puzzle_spec.rb
+++ Ruby/Sudoku/sudoku_puzzle_spec.rb
@@ -9,9 +9,15 @@ describe SudokuPuzzle do
   it "should allow the grid contents to be supplied on creation" do
     puzzle = SudokuPuzzle.new([[1,2], [2,1]])
     check_size(puzzle, 2)
+    puzzle = SudokuPuzzle.new([[1]])
+    check_size(puzzle, 1)
   end
 
   it "should only allow square grids" do
     lambda {SudokuPuzzle.new([[1,2]])}.should raise_error()
+  end
+
+  it "should not allow cell values greater than the puzzle size" do
+    lambda {SudokuPuzzle.new([[]])}.should raise_error()
   end
 end
