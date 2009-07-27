e33689051d3c97d027fe6ce73d75d76f97875639
Andreas Pauley <apauley+github@gmail.com>
2009-07-27 21:56:19 +0200

    Sudoku: first stab at separate algorithm

---
+++ Ruby/Sudoku/sudoku_algorithm.rb
@@ -0,0 +1,51 @@
+class SudokuAlgorithm
+  def initialize(puzzle)
+    @puzzle = puzzle
+    freeze
+  end
+
+  def improve_grid_component(an_array)
+    component_without_zeros = an_array.select {|each| each != 0}
+    if (@puzzle.size - component_without_zeros.size) > 1
+      return an_array
+    end
+
+    new_component = []
+    an_array.each {|x|
+      if x == 0
+        sum = @puzzle.sum(an_array)
+        new_component.push(@puzzle.component_total - sum)
+      else
+        new_component.push(x)
+      end
+    }
+    return new_component
+  end
+
+  def improve_components(an_array)
+    improved_components = []
+    an_array.each {|component|
+      new_component = improve_grid_component(component)
+      improved_components.push(new_component)
+    }
+    return improved_components
+  end
+
+  def solve
+    if @puzzle.solved?
+      return @puzzle
+    end
+
+    improved_rows = improve_components(@puzzle.rows)
+    improved_puzzle = SudokuPuzzle.new(improved_rows)
+
+    improved_columns = improve_components(improved_puzzle.columns)
+    improved_puzzle = SudokuPuzzle.columns(improved_columns)
+
+    if improved_puzzle == @puzzle
+      return @puzzle
+    else
+      return self.class.new(improved_puzzle).solve
+    end
+  end
+end
--- Ruby/Sudoku/sudoku_solver_spec.rb
+++ Ruby/Sudoku/sudoku_solver_spec.rb
@@ -7,28 +7,6 @@ describe SudokuSolver do
     solver.solve.object_id.should == puzzle.object_id
   end
 
-  it "should complete the last remaining element of a grid component" do
-    puzzle = SudokuPuzzle.empty(1)
-    solver = SudokuSolver.new(puzzle)
-    solver.improve_grid_component(puzzle.rows[0]).should == [1]
-
-    puzzle = SudokuPuzzle.new([[1,0], [0,0]])
-    solver = SudokuSolver.new(puzzle)
-    solver.improve_grid_component(puzzle.rows[0]).should == [1,2]
-
-    puzzle = SudokuPuzzle.new([[1,2,0], [2,0,0], [0,0,0]])
-    solver = SudokuSolver.new(puzzle)
-    solver.improve_grid_component(puzzle.rows[0]).should == [1,2,3]
-  end
-
-  it "should not change a row with too little info when trying to improve a grid component" do
-    puzzle = SudokuPuzzle.new([[0,3,0], [1,0,0], [0,0,0]])
-    solver = SudokuSolver.new(puzzle)
-    solver.improve_grid_component(puzzle.rows[0]).should == [0,3,0]
-    solver.improve_grid_component(puzzle.rows[1]).should == [1,0,0]
-    solver.improve_grid_component(puzzle.rows[2]).should == [0,0,0]
-  end
-
   it "should solve a 1x1 puzzle" do
     puzzle = SudokuPuzzle.new([[0]])
     solver = SudokuSolver.new(puzzle)
@@ -49,11 +27,5 @@ describe SudokuSolver do
     puzzle = SudokuPuzzle.new([[1,2,0], [2,0,0], [0,0,2]])
     solver = SudokuSolver.new(puzzle)
     solver.solve.should == SudokuPuzzle.new([[1,2,3], [2,3,1], [3,1,2]])
-  end
-
-  it "should do what it can on an impossible puzzle and quit" do
-    impossible_puzzle = SudokuPuzzle.new([[1,0,3], [0,0,0], [0,0,0]])
-    solver = SudokuSolver.new(impossible_puzzle)
-    solver.solve.should == SudokuPuzzle.new([[1,2,3], [0,0,0], [0,0,0]])
   end
 end
