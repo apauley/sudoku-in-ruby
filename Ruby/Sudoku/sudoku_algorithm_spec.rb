176a92dd0be90fddf4b530b917065b9ceacb0c9f
Andreas Pauley <apauley+github@gmail.com>
2009-08-02 22:28:14 +0200

    Sudoku: rip out CompleteLastRemainingAlgorithm

--- Ruby/Sudoku/sudoku_algorithm_spec.rb
+++ Ruby/Sudoku/sudoku_algorithm_spec.rb
@@ -31,32 +31,3 @@ describe TryYourLuckAlgorithm do
     newpuzzle.should == SudokuPuzzle.new([[1,2], [2,1]])
   end
 end
-
-describe CompleteLastRemainingAlgorithm do
-  it "should complete the last remaining element of a grid component" do
-    puzzle = SudokuPuzzle.empty(1)
-    algorithm = CompleteLastRemainingAlgorithm.new(puzzle)
-    algorithm.fill_remaining_element(puzzle.rows[0]).should == [1]
-
-    puzzle = SudokuPuzzle.new([[1,0], [0,0]])
-    algorithm = CompleteLastRemainingAlgorithm.new(puzzle)
-    algorithm.fill_remaining_element(puzzle.rows[0]).should == [1,2]
-
-    puzzle = SudokuPuzzle.new([[1,2,0], [2,0,0], [0,0,0]])
-    algorithm = CompleteLastRemainingAlgorithm.new(puzzle)
-    algorithm.fill_remaining_element(puzzle.rows[0]).should == [1,2,3]
-  end
-
-  it "should not change rows with too little info when trying to improve a grid component" do
-    rows = [[0,3,0], [1,0,0], [0,0,0]]
-    puzzle = SudokuPuzzle.new(rows)
-    algorithm = CompleteLastRemainingAlgorithm.new(puzzle)
-    algorithm.fill_components(rows).should == rows
-  end
-
-  it "should do what it can on a puzzle with too little info and quit" do
-    impossible_puzzle = SudokuPuzzle.new([[1,0,3], [0,0,0], [0,0,0]])
-    algorithm = CompleteLastRemainingAlgorithm.new(impossible_puzzle)
-    algorithm.solve.should == SudokuPuzzle.new([[1,2,3], [0,0,0], [0,0,0]])
-  end
-end
--- Ruby/Sudoku/sudoku_solver.rb
+++ Ruby/Sudoku/sudoku_solver.rb
@@ -2,13 +2,11 @@ require 'sudoku_algorithm'
 
 class SudokuSolver
   def initialize(puzzle)
-    @algorithm = CompleteLastRemainingAlgorithm.new(puzzle)
     @try_your_luck = TryYourLuckAlgorithm.new(puzzle)
     freeze
   end
 
   def solve
-    @algorithm.solve
     @try_your_luck.solve
   end
 end
