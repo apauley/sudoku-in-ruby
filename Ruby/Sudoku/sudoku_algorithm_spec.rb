8f54b80c461165768837b5031282cfa84d9cd1c4
Andreas Pauley <apauley+github@gmail.com>
2009-08-09 23:34:46 +0200

    Sudoku: move some specs to the algorithm

--- Ruby/Sudoku/sudoku_algorithm_spec.rb
+++ Ruby/Sudoku/sudoku_algorithm_spec.rb
@@ -54,4 +54,29 @@ describe RecursiveTrialAndErrorAlgorithm do
             [4,3,2,1]]
     newpuzzle.should == SudokuPuzzle.new(rows)
   end
+
+  it "should return a solved puzzle unchanged" do
+    puzzle = SudokuPuzzle.new([[1]])
+    algorithm = RecursiveTrialAndErrorAlgorithm.new(puzzle)
+    algorithm.solve.object_id.should == puzzle.object_id
+  end
+
+  it "should solve a 1x1 puzzle" do
+    puzzle = SudokuPuzzle.new([[0]])
+    algorithm = RecursiveTrialAndErrorAlgorithm.new(puzzle)
+    algorithm.solve.should == SudokuPuzzle.new([[1]])
+  end
+
+  it "should solve a 4x4 puzzle" do
+    puzzle = SudokuPuzzle.empty(4)
+    algorithm = RecursiveTrialAndErrorAlgorithm.new(puzzle)
+    solved_puzzle = algorithm.solve
+    rows = [[1,2,3,4],
+            [3,4,1,2],
+            [2,1,4,3],
+            [4,3,2,1]]
+
+    solved_puzzle.solved?.should == true
+    solved_puzzle.rows.should == rows
+  end
 end
--- Ruby/Sudoku/sudoku_solver_spec.rb
+++ Ruby/Sudoku/sudoku_solver_spec.rb
@@ -18,29 +18,4 @@ describe SudokuSolver do
     solver.to_s.include?("Algorithm:").should == true
     solver.to_s.include?("Crunched #{solver.crunched_puzzle}").should == true
   end
-
-  it "should return a solved puzzle unchanged" do
-    puzzle = SudokuPuzzle.new([[1]])
-    solver = SudokuSolver.new(puzzle)
-    solver.crunched_puzzle.object_id.should == puzzle.object_id
-  end
-
-  it "should solve a 1x1 puzzle" do
-    puzzle = SudokuPuzzle.new([[0]])
-    solver = SudokuSolver.new(puzzle)
-    solver.crunched_puzzle.should == SudokuPuzzle.new([[1]])
-  end
-
-  it "should solve a 4x4 puzzle" do
-    puzzle = SudokuPuzzle.empty(4)
-    solver = SudokuSolver.new(puzzle)
-    solved_puzzle = solver.crunched_puzzle
-    rows = [[1,2,3,4],
-            [3,4,1,2],
-            [2,1,4,3],
-            [4,3,2,1]]
-
-    solved_puzzle.solved?.should == true
-    solved_puzzle.rows.should == rows
-  end
 end
