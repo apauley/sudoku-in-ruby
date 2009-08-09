4f24884e01944e0ef982cc5d7694039b4c3f7462
Andreas Pauley <apauley+github@gmail.com>
2009-08-09 08:25:47 +0200

    Sudoku: refactor solver constructor

--- Ruby/Sudoku/sudoku_solver.rb
+++ Ruby/Sudoku/sudoku_solver.rb
@@ -8,15 +8,21 @@ class SudokuSolver
   def initialize(puzzle, algorithm_to_use="trial_and_error")
     @start_time = Time.now
     @algorithm = self.class.algorithms[algorithm_to_use].new(puzzle)
+    @crunched_puzzle = @algorithm.solve
+    @end_time = Time.now
     freeze
   end
 
   def solve(puzzle)
     results = {}
     results['start_time'] = @start_time
-    results['puzzle'] = @algorithm.solve
-    results['end_time'] = Time.now
-    results['elapsed_time'] = results['end_time'] - results['start_time']
+    results['puzzle'] = @crunched_puzzle
+    results['end_time'] = @end_time
+    results['elapsed_time'] = elapsed_time
     return results
+  end
+
+  def elapsed_time
+    @end_time - @start_time
   end
 end
