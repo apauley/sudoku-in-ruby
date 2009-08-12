1fdbf07ab329bb06f23275a275cd78c46188d403
Andreas Pauley <apauley+github@gmail.com>
2009-08-12 23:54:46 +0200

    Sudoku: move stats keeper

---
+++ Ruby/Sudoku/stats_keeper.rb
@@ -0,0 +1,43 @@
+class SolverStatsKeeper
+  def initialize
+    @start_time = Time.now
+    @valid_attempts = 0
+    @error_attempts = 0
+  end
+
+  def valid_attempt!
+    @valid_attempts = @valid_attempts + 1
+  end
+
+  def error_attempt!
+    @error_attempts = @error_attempts + 1
+  end
+
+  def end_time!
+    @end_time = Time.now
+  end
+
+  def start_time
+    @start_time
+  end
+
+  def end_time
+    @end_time
+  end
+
+  def elapsed_time
+    end_time - start_time
+  end
+
+  def valid_attempts
+    @valid_attempts
+  end
+
+  def error_attempts
+    @error_attempts
+  end
+
+  def total_attempts
+    @valid_attempts + @error_attempts
+  end
+end
